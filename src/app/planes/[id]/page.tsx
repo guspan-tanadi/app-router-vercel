fdd32bc2b4a8b0d06459b25f4103192e7813874a
guspan-tanadi <guspan.tanadi@gmail.com>
2024-11-18 21:01:40 +0700

    chore: push

--- src/app/planes/[id]/page.tsx
+++ src/app/planes/[id]/page.tsx
@@ -4,19 +4,18 @@ import { useParams } from "next/navigation";
 interface Params {
     id?: string;
 }
-  
-  const SlugWithIdPage = () => {
+
+const SlugWithIdPage = () => {
     const { id } = useParams() as Params;
-  
+
     return (
-      <main>
-        <h1>Slug with ID Page</h1>
-        <p>
-          You with ID: <strong>{id}</strong>
-        </p>
-      </main>
+        <main>
+            <h1>Slug with ID Page</h1>
+            <p>
+                You with ID: <strong>{id}</strong>
+            </p>
+        </main>
     );
-  };
-  
-  export default SlugWithIdPage;
-  
+};
+
+export default SlugWithIdPage;
