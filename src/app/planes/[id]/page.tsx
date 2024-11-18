4d772f7df502e2fc84dd58c86f922cf6aabe5ae0
guspan-tanadi <guspan.tanadi@gmail.com>
2024-11-18 20:54:13 +0700

    init

---
+++ src/app/planes/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { useParams } from "next/navigation";
+
+interface Params {
+    id?: string;
+}
+  
+  const SlugWithIdPage = () => {
+    const { id } = useParams() as Params;
+  
+    return (
+      <main>
+        <h1>Slug with ID Page</h1>
+        <p>
+          You with ID: <strong>{id}</strong>
+        </p>
+      </main>
+    );
+  };
+  
+  export default SlugWithIdPage;
+  
